798054272cdaaa9da986e06e0118b55a7031764b
Patrick Rueschenbaum <patrick.rueschenbaum@googlemail.com>
2020-04-22 11:43:09 +0200

    Changed config.json path to use source root

--- src/main/java/com/baqend/Benchmark.java
+++ src/main/java/com/baqend/Benchmark.java
@@ -14,7 +14,7 @@ public class Benchmark {
         System.out.println("+++ Benchmark started +++");
 
         Gson gson = new Gson();
-        ConfigObject configObject = gson.fromJson(new FileReader("C:\\Users\\RüschenbaumPatrickIn\\IdeaProjects\\rtdb-sp-benchmark\\src\\main\\java\\com\\baqend\\config.json"), ConfigObject.class);
+        ConfigObject configObject = gson.fromJson(new FileReader("src\\main\\java\\com\\baqend\\config.json"), ConfigObject.class);
         System.out.println("Configuration file loaded");
 
         Client client;
