e21b514ade65d4d5df466705f30cb7f97a107fdb
Patrick Rueschenbaum <patrick.rueschenbaum@googlemail.com>
2020-05-12 15:44:52 +0200

    Refactored and added percentile messages to display

--- src/main/java/com/baqend/core/LoadGenerator.java
+++ src/main/java/com/baqend/core/LoadGenerator.java
@@ -31,10 +31,11 @@ public class LoadGenerator {
     }
 
     public void start() {
-        System.out.println("Benchmark started");
         double x = 1;
         double rounds = 60;
-        int throughput = 2000; // ops/s
+        int throughput = 2500; // ops/s
+
+        System.out.println("Benchmark started - Throughput: " + throughput + " ops/s");
         double startTime = System.currentTimeMillis();
         while (x <= rounds) {
             System.out.print("\rBenchmark in progess: " + (int) (x / rounds * 100) + "%");
@@ -52,27 +53,30 @@ public class LoadGenerator {
         double executionTime = stopTime - startTime;
         System.out.println();
         System.out.println("Benchmark done - Execution Time: " + executionTime + "ms");
-        System.out.println("Throughput: " + throughput + " ops/s");
         try {
-            Thread.sleep(5000);
+            Thread.sleep(15000);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void stop() {
-        JsonExporter jsonExporter = new JsonExporter();
         System.out.println("Quantitative Correctness: " + LatencyMeasurement.getInstance().getQuantitativeCorrectness());
         System.out.println("Average: " + LatencyMeasurement.getInstance().calculateAverage() + "ms");
         System.out.println("Median: " + LatencyMeasurement.getInstance().calculateMedian() + "ms");
         System.out.println("Maximum: " + LatencyMeasurement.getInstance().getMaximumLatency() + "ms");
         System.out.println("Minimum: " + LatencyMeasurement.getInstance().getMinimumLatency() + "ms");
+        System.out.println("90th Percentile: " + LatencyMeasurement.getInstance().calculateNthPercentile(0.9) + "ms");
+        System.out.println("95th Percentile: " + LatencyMeasurement.getInstance().calculateNthPercentile(0.95) + "ms");
+        System.out.println("99th Percentile: " + LatencyMeasurement.getInstance().calculateNthPercentile(0.99) + "ms");
         Map<UUID, Long> latencies = LatencyMeasurement.getInstance().calculateAllLatencies();
+        JsonExporter jsonExporter = new JsonExporter();
         try {
             jsonExporter.exportToJsonFile(latencies);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        client.cleanUp();
     }
 
     public void step() {
@@ -83,8 +87,8 @@ public class LoadGenerator {
 
         LatencyMeasurement.getInstance().tick(transactionID);
         client.insert("Test", transactionID.toString(), values, transactionID);
-        //client.update("Test", "bbdc4d81-bbc0-4500-bec3-8d069b5ae2ad", values, transactionID);
-        //client.delete("Test", "4eb13cec-b9b4-4ebb-98db-99c2d65c7ae5", transactionID);
+        //client.update("Test", "7e1df1e5-c9fb-457a-9efe-48a543a4dd2e", values, transactionID);
+        //client.delete("Test", "7e1df1e5-c9fb-457a-9efe-48a543a4dd2e", transactionID);
     }
 
 }
