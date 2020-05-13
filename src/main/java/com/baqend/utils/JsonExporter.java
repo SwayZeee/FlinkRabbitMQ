d40b2461b8523e8a20e9d035510dbc0f798fdda9
Patrick Rueschenbaum <patrick.rueschenbaum@googlemail.com>
2020-05-13 15:21:35 +0200

    Adjustments to export initialLoad.json

--- src/main/java/com/baqend/utils/JsonExporter.java
+++ src/main/java/com/baqend/utils/JsonExporter.java
@@ -1,12 +1,13 @@
 package com.baqend.utils;
 
+import com.baqend.workload.LoadData;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class JsonExporter {
@@ -16,12 +17,20 @@ public class JsonExporter {
         gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
-    public void exportToJsonFile(Map<UUID, Long> data) throws IOException {
+    public void exportLatenciesToJsonFile(HashMap<UUID, Long> data) throws IOException {
         long timeStamp = System.currentTimeMillis();
         Writer writer = new FileWriter("src\\main\\java\\com\\baqend\\results\\" + timeStamp + ".json");
         gson.toJson(data, writer);
         writer.flush();
         writer.close();
         System.out.println("Results exported: " + timeStamp + ".json");
+    }
+
+    public void exportInitialLoadToJsonFile(LoadData data) throws IOException {
+        Writer writer = new FileWriter("src\\main\\java\\com\\baqend\\workload\\initialLoad.json");
+        gson.toJson(data, writer);
+        writer.flush();
+        writer.close();
+        System.out.println("Initial load exported: initialLoad.json");
     }
 }
