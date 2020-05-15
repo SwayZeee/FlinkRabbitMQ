d502f34821eba012fcdd2b1356d768af0ec9d51d
Patrick Rueschenbaum <patrick.rueschenbaum@googlemail.com>
2020-05-15 16:40:33 +0200

    Added throughput, duration and waitingTime to config file

--- src/main/java/com/baqend/config/ConfigObject.java
+++ src/main/java/com/baqend/config/ConfigObject.java
@@ -4,4 +4,7 @@ public class ConfigObject {
     public int clientToTest;
     public String baqendWebsocketUri;
     public String baqendHttpBaseUri;
+    public int throughput;
+    public int duration;
+    public int waitingTime;
 }
