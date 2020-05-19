e28f9f847c6c96ae119612455d28678adba1d612
SwayZeee <patrick.rueschenbaum@googlemail.com>
2020-05-19 13:34:52 +0200

    Added RateLimitter and removed LatencyMeasurement for RMQLatencySender

--- src/main/java/com/baqend/core/LoadGenerator.java
+++ src/main/java/com/baqend/core/LoadGenerator.java
@@ -2,24 +2,26 @@ package com.baqend.core;
 
 import com.baqend.client.Client;
 import com.baqend.config.ConfigObject;
-import com.baqend.utils.JsonExporter;
+import com.baqend.messaging.RMQLatencySender;
 import com.baqend.utils.RandomDataGenerator;
 import com.baqend.workload.LoadData;
+import com.google.common.util.concurrent.RateLimiter;
 import com.google.gson.Gson;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
 public class LoadGenerator {
 
     private final Client client;
     private final ConfigObject configObject;
     private final RandomDataGenerator randomDataGenerator = new RandomDataGenerator();
+    private final RMQLatencySender rmqLatencySender = new RMQLatencySender();
 
-    public LoadGenerator(Client client, ConfigObject configObject) {
+    public LoadGenerator(Client client, ConfigObject configObject) throws IOException, TimeoutException {
         this.client = client;
         this.configObject = configObject;
     }
@@ -41,63 +43,53 @@ public class LoadGenerator {
     }
 
     public void start() {
+        RateLimiter rateLimiter = RateLimiter.create(configObject.throughput);
         double x = 1;
-        double rounds = configObject.duration;
+        int rounds = configObject.duration;
         int throughput = configObject.throughput;
 
         System.out.println("Benchmark started - Throughput: " + throughput + " ops/s");
         double startTime = System.currentTimeMillis();
-        while (x <= rounds) {
-            System.out.print("\rBenchmark in progess: " + (int) (x / rounds * 100) + "%");
-            for (int i = 0; i < throughput; i++) {
-                CompletableFuture.runAsync(this::step);
-            }
+        while (x <= rounds * throughput) {
+            System.out.print("\rBenchmark in progess: " + (int) (x / (rounds * throughput) * 100) + " %");
+            rateLimiter.acquire();
+            step((int) x);
             x++;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
+
         double stopTime = System.currentTimeMillis();
         double executionTime = stopTime - startTime;
         System.out.println();
-        System.out.println("Benchmark done - Execution Time: " + executionTime + "ms");
+        System.out.println("Benchmark done - Execution Time: " + executionTime + " ms");
         try {
             Thread.sleep(configObject.waitingTime);
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void step() {
+    public void step(int count) {
         UUID transactionID = UUID.randomUUID();
-        LatencyMeasurement.getInstance().tick(transactionID);
-        client.insert("Test", transactionID.toString(), randomDataGenerator.generateRandomDataset(), transactionID);
-        //client.update("Test", "7e1df1e5-c9fb-457a-9efe-48a543a4dd2e", randomDataGenerator.generateRandomDataset(), transactionID);
+        if (count % (configObject.throughput / 100) == 0) {
+            try {
+                rmqLatencySender.sendMessage("tick" + "," + 0 + "," + transactionID.toString() + "," + System.nanoTime());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            client.insert("Test", transactionID.toString(), randomDataGenerator.generateRandomDataset(1), transactionID);
+            //client.update("Test", "7e1df1e5-c9fb-457a-9efe-48a543a4dd2e", randomDataGenerator.generateRandomDataset(1), transactionID);
+            return;
+        }
+        client.insert("Test", transactionID.toString(), randomDataGenerator.generateRandomDataset(0), transactionID);
+        //client.update("Test", "7e1df1e5-c9fb-457a-9efe-48a543a4dd2e", randomDataGenerator.generateRandomDataset(0), transactionID);
         //client.delete("Test", "7e1df1e5-c9fb-457a-9efe-48a543a4dd2e", transactionID);
     }
 
     public void stop() {
         client.cleanUp("Test");
-        doCalculationsAndExport();
+        rmqLatencySender.close();
     }
 
-    public void doCalculationsAndExport() {
-        System.out.println("Quantitative Correctness: " + LatencyMeasurement.getInstance().getQuantitativeCorrectness());
-        System.out.println("Average: " + LatencyMeasurement.getInstance().calculateAverage() + "ms");
-        System.out.println("Median: " + LatencyMeasurement.getInstance().calculateMedian() + "ms");
-        System.out.println("Maximum: " + LatencyMeasurement.getInstance().getMaximumLatency() + "ms");
-        System.out.println("Minimum: " + LatencyMeasurement.getInstance().getMinimumLatency() + "ms");
-        System.out.println("90th Percentile: " + LatencyMeasurement.getInstance().calculateNthPercentile(0.9) + "ms");
-        System.out.println("95th Percentile: " + LatencyMeasurement.getInstance().calculateNthPercentile(0.95) + "ms");
-        System.out.println("99th Percentile: " + LatencyMeasurement.getInstance().calculateNthPercentile(0.99) + "ms");
-        HashMap<UUID, Long> latencies = LatencyMeasurement.getInstance().calculateAllLatencies();
-        JsonExporter jsonExporter = new JsonExporter();
-        try {
-            jsonExporter.exportLatenciesToJsonFile(latencies);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 }
