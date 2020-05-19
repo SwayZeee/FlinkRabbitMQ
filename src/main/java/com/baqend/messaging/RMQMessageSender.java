ebc76ab1ac759f5369f7dce34d6d7632adb946de
SwayZeee <patrick.rueschenbaum@googlemail.com>
2020-05-19 13:31:35 +0200

    Refactored to not use singleton pattern

--- src/main/java/com/baqend/messaging/RMQMessageSender.java
+++ src/main/java/com/baqend/messaging/RMQMessageSender.java
@@ -8,37 +8,43 @@ import java.util.concurrent.TimeoutException;
 
 public class RMQMessageSender {
 
-    private static RMQMessageSender single_instance = null;
     private final Connection connection;
     private final Channel channel;
     private final String EXCHANGE_NAME = "benchmark";
 
-    private RMQMessageSender() throws IOException, TimeoutException {
+    public RMQMessageSender() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         connection = factory.newConnection();
         channel = connection.createChannel();
-
         channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-    }
-
-    public static RMQMessageSender getInstance() throws IOException, TimeoutException {
-        if (single_instance == null) {
-            single_instance = new RMQMessageSender();
-        }
-        return single_instance;
     }
 
     public void sendMessage(String message) throws IOException {
         channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
-        // System.out.println(" [x] Sent '" + message + "'");
+        //System.out.println("[x] Sent '" + message + "'");
     }
 
-    public void closeChannel() throws IOException, TimeoutException {
-        channel.close();
+    public void closeChannel() {
+        try {
+            channel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void closeConnection() throws IOException {
-        connection.close();
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        closeChannel();
+        closeConnection();
     }
 }
