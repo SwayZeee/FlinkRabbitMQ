9a6217558e9e55a71fbee825250c17fcf2e76bda
Patrick Rueschenbaum <patrick.rueschenbaum@googlemail.com>
2020-05-21 18:29:28 +0200

    Added new fields to config file and ConfigObject

--- src/main/java/com/baqend/config/ConfigObject.java
+++ src/main/java/com/baqend/config/ConfigObject.java
@@ -2,9 +2,10 @@ package com.baqend.config;
 
 public class ConfigObject {
     public int clientToTest;
-    public String baqendWebsocketUri;
-    public String baqendHttpBaseUri;
     public int throughput;
     public int duration;
     public int waitingTime;
+    public boolean isPerformingLoad;
+    public boolean isPerformingWarmUp;
+    public boolean isPerformingCleanUp;
 }
