ede39ced992cd81ab6a9234770b2d371eab30828
SwayZeee <patrick.rueschenbaum@googlemail.com>
2020-05-21 11:11:39 +0200

    Changes to enable initial query result time

--- src/main/java/com/baqend/core/QueryOrchestrator.java
+++ src/main/java/com/baqend/core/QueryOrchestrator.java
@@ -1,26 +1,36 @@
 package com.baqend.core;
 
 import com.baqend.client.Client;
+import com.baqend.messaging.RMQLatencySender;
 import com.baqend.query.Query;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
 public class QueryOrchestrator {
 
+    private final RMQLatencySender rmqLatencySender = new RMQLatencySender();
     private final Query query;
     private final Client client;
 
-    public QueryOrchestrator(Query query, Client client) {
+    public QueryOrchestrator(Query query, Client client) throws IOException, TimeoutException {
         this.query = query;
         this.client = client;
     }
 
     public void subscribeQuery() {
-        //UUID uuid = UUID.randomUUID();
-        //LatencyMeasurement.getInstance().tick(uuid);
-        //LatencyMeasurement.getInstance().setInitialTick(uuid, System.nanoTime());
+        UUID uuid = UUID.randomUUID();
+        try {
+            rmqLatencySender.sendMessage("tick" + "," + 1 + "," + uuid.toString() + "," + System.nanoTime());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         client.subscribeQuery(query.getQuery());
     }
 
     public void unsubscribeQuery() {
+        rmqLatencySender.close();
         client.unsubscribeQuery();
     }
 }
