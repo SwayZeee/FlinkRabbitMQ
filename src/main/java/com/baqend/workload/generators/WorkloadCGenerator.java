c46f29589f8b4786f33af203c2ebc0cbf35e2ee5
Patrick Rueschenbaum <patrick.rueschenbaum@googlemail.com>
2020-05-28 19:19:05 +0200

    Added variable to adjust the query amount

--- src/main/java/com/baqend/workload/generators/WorkloadCGenerator.java
+++ src/main/java/com/baqend/workload/generators/WorkloadCGenerator.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -34,37 +35,41 @@ public class WorkloadCGenerator {
 
     public static QuerySet generateQuerySet() {
         QuerySet querySet = new QuerySet();
-        Query numberQuery1 = new Query("{ $and: [ { \\\"number\\\": { $gt: 0 } }, { \\\"number\\\": { $lt: 501 } } ] }", "");
-        querySet.addQuery(numberQuery1);
-        Query numberQuery2 = new Query("{ $and: [ { \\\"number\\\": { $gt: 500 } }, { \\\"number\\\": { $lt: 1001 } } ] }", "");
-        querySet.addQuery(numberQuery2);
-        Query numberQuery3 = new Query("{ $and: [ { \\\"number\\\": { $gt: 1000 } }, { \\\"number\\\": { $lt: 1501 } } ] }", "");
-        querySet.addQuery(numberQuery3);
-        Query numberQuery4 = new Query("{ $and: [ { \\\"number\\\": { $gt: 1500 } }, { \\\"number\\\": { $lt: 2001 } } ] }", "");
-        querySet.addQuery(numberQuery4);
-        Query numberQuery5 = new Query("{ $and: [ { \\\"number\\\": { $gt: 2000 } }, { \\\"number\\\": { $lt: 2501 } } ] }", "");
-        querySet.addQuery(numberQuery5);
+        int queryAmount = 10;
+        for (int i = 0; i <= queryAmount; i++) {
+            Query numberQuery = new Query("{ $and: [ { \\\"number\\\": { $gt: " + 500 * i + " } }, { \\\"number\\\": { $lt: " + (500 * (i + 1) + 1) + " } } ] }", "");
+            querySet.addQuery(numberQuery);
+        }
         return querySet;
     }
+
+    ArrayList<UUID> forbiddenIDs = new ArrayList<UUID>();
 
     public static Workload generateWorkload(int duration, int throughput, int insertProportion, int updateProportion) throws FileNotFoundException {
         Workload initialWorkloadData = gson.fromJson(new FileReader("src\\main\\java\\com\\baqend\\generated\\workloads\\initialLoad.json"), Workload.class);
         LoadData relevantTupels = new LoadData();
         LoadData irrelevantTupels = new LoadData();
         Workload workload = new Workload();
+        int queryAmount = 10;
 
         for (WorkloadEvent workloadEvent : initialWorkloadData.getWorkload()) {
             // TODO: perform check for relevant data tupels in initial load
-            if (Integer.parseInt(workloadEvent.getSingleDataSet().getData().get("number")) <= 2500) {
+            if (Integer.parseInt(workloadEvent.getSingleDataSet().getData().get("number")) <= 500 * queryAmount) {
                 relevantTupels.addSingleDataSet(workloadEvent.getSingleDataSet());
             } else {
                 irrelevantTupels.addSingleDataSet(workloadEvent.getSingleDataSet());
             }
         }
 
+        ArrayList<UUID> forbiddenIDs = new ArrayList<UUID>();
+
         for (int i = 1; i <= (duration * throughput); i++) {
             int randomNumber = randomDataGenerator.generateRandomInteger(1, 100);
             UUID transactionID = UUID.randomUUID();
+
+            if (forbiddenIDs.size() == throughput) {
+                forbiddenIDs.remove(0);
+            }
             // measurement relevant events
             if (i % (throughput / 100) == 0) {
                 // updates only, no relevant inserts or deletes
@@ -75,6 +80,8 @@ public class WorkloadCGenerator {
 
                 WorkloadEvent newWorkloadEvent = new WorkloadEvent(transactionID, WorkloadEventType.UPDATE, true, newSingleDataSet);
                 workload.addWorkloadEvent(newWorkloadEvent);
+
+                forbiddenIDs.add(newSingleDataSet.getUuid());
             } else {
                 if (randomNumber <= insertProportion) {
                     HashMap<String, String> data = randomDataGenerator.generateRandomDataset(initialWorkloadData.getWorkload().size() + 1);
@@ -84,6 +91,8 @@ public class WorkloadCGenerator {
                     WorkloadEvent newWorkloadEvent = new WorkloadEvent(transactionID, WorkloadEventType.INSERT, false, newSingleDataSet);
                     initialWorkloadData.addWorkloadEvent(newWorkloadEvent);
                     workload.addWorkloadEvent(newWorkloadEvent);
+
+                    forbiddenIDs.add(newSingleDataSet.getUuid());
                 } else if (randomNumber <= insertProportion + updateProportion) {
                     int randomIndex = randomDataGenerator.generateRandomInteger(0, irrelevantTupels.getLoad().size() - 1);
                     SingleDataSet singleDataSet = irrelevantTupels.getLoad().get(randomIndex);
@@ -92,6 +101,8 @@ public class WorkloadCGenerator {
 
                     WorkloadEvent newWorkloadEvent = new WorkloadEvent(transactionID, WorkloadEventType.UPDATE, false, newSingleDataSet);
                     workload.addWorkloadEvent(newWorkloadEvent);
+
+                    forbiddenIDs.add(newSingleDataSet.getUuid());
                 }
             }
         }
